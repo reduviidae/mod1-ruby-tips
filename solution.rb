d1527cc1b23f7688ca4900d4d7a84ed7236a0210
reduviidae <wiljago@gmail.com>
2018-12-19 13:56:01 -0500

    started bonus

--- solution.rb
+++ solution.rb
@@ -5,4 +5,21 @@ def palindrome?(string)
   string == backwards
 end
 
+def palindrome_bonus(string)
+  if string[-1] == string[0]
+    if string[-2] == string[1]
+      if string[-3] == string[2]
+        true
+      else
+        false
+      end
+    else
+      false
+    end
+  else
+    false
+  end
+
+end
+
 binding.pry
